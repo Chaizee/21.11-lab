c06994c9c9db92142080d7cc85bbf6ff136216ac
Chaizee <zuzindima26@gmail.com>
2024-11-21 10:03:47 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Определить количество строк, в которых присутствуют сочетания a*b, где * - любой символ.
+class lab
+{
+    public static void Main(string[] args)
+    {
+       string n = Console.ReadLine();
+       int Count = 0;
+       while (n != "")
+       {
+            n = n.ToLower();
+            for (int i = 0; i < n.Length-2; i++)
+            {
+                if (n[i] == 'a' && n[i+2] == 'b')
+                {
+                    Count++;
+                }
+            }
+            n = Console.ReadLine();
+        }
+       Console.WriteLine(Count);
+    }
+}
+
+//Определить максимальную длину подстроки в каждой строе, состоящей из сочетания abc
